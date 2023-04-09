cbd61be023aaa7df4cce5a513bd9b17c3d6e672f
bw_esc13 <barry.walker@esc13.txed.net>
2023-04-09 15:19:40 -0500

    Committing early phase of project - a few components built, some basic functionality

---
+++ src/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const searchImages = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+        headers: {
+            Authorization: 'Client-ID quzgu4Cgf62LgazDEKXamUY2GiWcT2ixXxrnl9nbYEM'
+        },
+        params: {
+            query: term,
+        }
+    });
+
+    return response.data.results;
+}
+export default searchImages;
